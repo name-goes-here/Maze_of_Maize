37bdccd815a780df02f4f7620807306c15733884
Brandon Hernandez <starkiller127.bh@gmail.com>
2019-04-27 23:17:02 -0700

    Basic movement script

---
+++ Movement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    //Change speed variable in unity editor during testing. No movement will happen at the dfault value (zero).
+    public float speed;
+
+    void Update(){
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+    }
+}
