788308760e38c364bcb998ee6dfce68d3040a54b
CheBlanco <ocelotking999@gmail.com>
2019-05-01 14:48:26 -0700

    when micheal helped us

--- Tileset Test/Assets/Movement.cs
+++ Tileset Test/Assets/Movement.cs
@@ -5,26 +5,34 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
     //Change speed variable in unity editor during testing. No movement will happen at the default value (zero).
-    public float speed;
-    Vector3 pos;
+    public float speed = 5f;
+    //Vector3 pos;
 
     private void Start() {
-        pos = transform.position;
+       // pos = transform.position;
     }
 
     void Update(){
         //transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
-
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) && transform.position == pos) {
-            pos += Vector3.right;
-        } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) && transform.position == pos) {
-            pos += Vector3.left;
-        } else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) && transform.position == pos) {
-            pos += Vector3.up;
-        } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) && transform.position == pos) {
-            pos += Vector3.down;
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+        float horz =Input.GetAxis("Horizontal");
+        float vert =Input.GetAxis("Vertical");
+        
+        // POSITION SETTING
+        //transform.position += (new Vector3(horz, vert, 0) * speed * Time.deltaTime);
+        
+        // VELOCITY SETTING
+        GetComponent<Rigidbody2D>().velocity = (new Vector3(horz, vert, 0) * speed);
+        
+//        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) && transform.position == pos) {
+//            pos += Vector3.right;
+//        } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) && transform.position == pos) {
+//            pos += Vector3.left;
+//        } else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) && transform.position == pos) {
+//            pos += Vector3.up;
+//        } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) && transform.position == pos) {
+//            pos += Vector3.down;
+//        }
+//
+//        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
     }
 }
