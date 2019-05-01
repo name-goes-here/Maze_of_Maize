66438df41cb6c3cb41475a823b899cd904864e2e
name-goes-here <starkiller127.bh@gmail.com>
2019-04-30 23:17:27 -0700

    Fine tune motion
    Movement now fits style of game more

--- Movement.cs
+++ Movement.cs
@@ -1,13 +1,30 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-    //Change speed variable in unity editor during testing. No movement will happen at the dfault value (zero).
+    //Change speed variable in unity editor during testing. No movement will happen at the default value (zero).
     public float speed;
+    Vector3 pos;
+
+    private void Start() {
+        pos = transform.position;
+    }
 
     void Update(){
-        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+        //transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.D) && transform.position == pos) {
+            pos += Vector3.right;
+        } else if (Input.GetKeyDown(KeyCode.A) && transform.position == pos) {
+            pos += Vector3.left;
+        } else if (Input.GetKeyDown(KeyCode.W) && transform.position == pos) {
+            pos += Vector3.up;
+        } else if (Input.GetKeyDown(KeyCode.S) && transform.position == pos) {
+            pos += Vector3.down;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
     }
 }
