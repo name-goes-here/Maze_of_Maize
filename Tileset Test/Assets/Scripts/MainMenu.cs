851b5837c0cdb90f23dcc3036a19f2372384d6ec
Brandon Hernandez <starkiller127.bh@gmail.com>
2019-05-03 00:28:41 -0700

    Transition to winning scene on goal reach

---
+++ Tileset Test/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void win() {
+        SceneManager.LoadScene("Winning Screen");
+    }
+
+    public void play() {
+        SceneManager.LoadScene("MazeTest");
+    }
+}
