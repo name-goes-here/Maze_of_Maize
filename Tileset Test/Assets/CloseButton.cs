86124d1ae8d1860d1fbd6141bf11204a0b4808f3
CheBlanco <ocelotking999@gmail.com>
2019-05-03 11:35:07 -0700

    start menu

---
+++ Tileset Test/Assets/CloseButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This will close the game
+public class CloseButton : MonoBehaviour
+{
+    
+    public void doExitGame() 
+    {
+    Application.Quit();
+    Debug.Log("quits game");
+    }
+}
---
+++ Tileset Test/Assets/spritefont_and_other/CreditsToMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsToMainMenu : MonoBehaviour
+{
+    
+
+
+    public void accessCredits()
+    {
+   
+        SceneManager.LoadScene("MazeTest",LoadSceneMode.Single);
+    }
+
+  
+}
