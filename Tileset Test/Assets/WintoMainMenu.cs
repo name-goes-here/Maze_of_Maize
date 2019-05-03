20dac5279c2dc771cdedf506bb783997e9a18221
CheBlanco <ocelotking999@gmail.com>
2019-05-03 11:51:04 -0700

    button swork

---
+++ Tileset Test/Assets/WintoMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WintoMainMenu : MonoBehaviour
+{
+    
+
+
+    public void accessCredits()
+    {
+   
+        SceneManager.LoadScene("MenuScreen",LoadSceneMode.Single);
+    }
+
+  
+}
